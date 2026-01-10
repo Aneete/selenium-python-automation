2fe016a2352ec07023eebad209b3e49518d9f0c7
Aneete <aneete@voxtronme.com>
2026-01-11 00:07:53 +0530

    basics

---
+++ python_basics/operators.py
@@ -0,0 +1,53 @@
+#Arithmetic
+
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+
+#assignment
+
+a+=5
+print(a)
+a-=3
+print(a)
+a*=2
+print(a)
+a/=4
+print(a)
+
+# relational operators
+print(a==b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+
+x="10"
+print(type(x))
+
+#Fix this code:print("Attempt " + 3)
+
+print("attempt",3)
+
+# converting"15.5" to float and print
+x="15.5"
+print(type(x))
+y=float(x)
+print(y)
+
+#Take user input for status code and compare with 200
+a = int(input("Enter the status code"))
+print(a)
+print(type(a))
+print(a==200)
+
+
+
+
