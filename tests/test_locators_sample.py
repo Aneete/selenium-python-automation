67fa7ad1bc2932ac18390731aa46b06e5d3d6dbf
Aneete <aneete@voxtronme.com>
2026-01-22 09:26:27 +0530

    locators sample

---
+++ tests/test_locators_sample.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+from python_basics.strings import username
+
+
+def test_login_page():
+    driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
+    driver.get("https://the-internet.herokuapp.com/login")
+    driver.maximize_window()
+
+    username = driver.find_element(By.ID,"username")
+    username.send_keys("tomsmith")
+    password = driver.find_element(By.ID, "password")
+    password.send_keys("SuperSecretPassword!")
+    login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+    login_button.click()
+    time.sleep(3)
+    success_msg = driver.find_element(By.ID,"flash").text
+    assert"You logged into a secure area!" in success_msg
+    driver.quit()
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,3 +1,15 @@
-def testbasic():
-    print("Automation project setup test")
-    assert True
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def test_open_google():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://www.google.com")
+    time.sleep(3)
+    title = driver.title
+    assert "Google" in title
+    driver.quit()
