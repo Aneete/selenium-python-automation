a19d740fa62172989ae86e511d321feb82f6d317
Aneete <aneete@voxtronme.com>
2026-02-03 17:55:13 +0530

    login test - POM

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
@@ -9,13 +12,16 @@ class LoginPage:
     message_text = (By.ID, "flash")
 
     def enter_username(self, username):
-        self.driver.find_element(self.username_field).send_keys(username)
+        self.driver.find_element(*self.username_field).send_keys(username)
 
     def click_login(self):
         self.driver.find_element(*self.login_button).click()
 
     def get_message(self):
-        return self.driver.find_element(*self.message_text).text
+        wait = WebDriverWait(self.driver, 10)
+        return wait.until(
+            EC.visibility_of_element_located(self.message_text)
+        ).text
 
     def enter_password(self, password):
-        self.driver.find_element(self.password_field).send_keys(password)
+        self.driver.find_element(*self.password_field).send_keys(password)
